d553bb59373409c9fabf8d2e234367b04cf2c450
Desipientia <daria.shonova@gmail.com>
2017-01-23 19:36:28 +1000

    Set more useful parameters

--- LineDetection/main.cpp
+++ LineDetection/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]) {
     size_t from, to;
     string filename, saveFilename;
     Mat source, edge, colorEdge;
-    vector<Vec4i> lines1, lines2;
+    vector<Vec4i> lines, lines1, lines2;
 
     filename = argc >= 2 ? argv[1] : "../Images/01.jpg";
     source = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
@@ -22,27 +22,21 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    Canny(source, edge, 50, 200, 3);
+    Canny(source, edge, 60, 90, 3);
     cvtColor(edge, colorEdge, CV_GRAY2BGR);
 
-    HoughLinesP(edge, lines1, 1, CV_PI/180, 80, 5, 2);
-    HoughLinesP(edge, lines2, 1, CV_PI/180, 80, 50, 2);
+    HoughLinesP(edge, lines1, 1, CV_PI/180, 100, 100, 10);
 
     for (size_t i = 0; i < lines1.size(); i++) {
         line(colorEdge, Point(lines1[i][0], lines1[i][1]),
-            Point(lines1[i][2], lines1[i][3]), Scalar(0, 0, 255), 2, 8);
-    }
-
-    for (size_t i = 0; i < lines2.size(); i++) {
-        line(colorEdge, Point(lines2[i][0], lines2[i][1]),
-            Point(lines2[i][2], lines2[i][3]), Scalar(255, 0, 0), 1, 8);
+            Point(lines1[i][2], lines1[i][3]), Scalar(0, 0, 255), 3, 8);
     }
 
     from = filename.find_last_of("/") + 1;
     to = filename.find_last_of(".");
     saveFilename = filename.substr(from, to - from);
 
-    imwrite("Result/" + saveFilename + ".jpg", source);
+    imwrite("Result/" + saveFilename + "_.jpg", source);
     imwrite("Result/" + saveFilename + "_lines.jpg", colorEdge);
 
     return 0;
